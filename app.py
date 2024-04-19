f8722821b072b4c5e86a545f965c84f17ec7d8ef
thuanvo09 <thuanvo09@hotmail.com>
2024-04-19 06:55:46 -0400

    fixed port

--- app.py
+++ app.py
@@ -208,4 +208,4 @@ if __name__ == '__main__':
                 shutil.rmtree(os.path.join(root, name))
 
     app.debug = True
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=8080)
